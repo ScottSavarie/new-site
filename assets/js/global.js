05ed6c276c9e9c99c76d56c7c5aaf705a1f3fc19
Scott Savarie <scottsavarie@gmail.com>
2012-11-20 23:38:15 +0100

    Website git hub update

---
+++ assets/js/global.js
@@ -0,0 +1,12 @@
+/* Javascript for Integer */
+
+/* =jQuery
+============================================================================== */
+
+	
+$(document).ready(function() {
+$("#header, #intro").hide();
+$('#header, #intro').fadeIn('slow','linear');
+
+}); // end document ready
+
