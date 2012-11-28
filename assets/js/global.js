dd421db428701fd583329985bc269c951d139cf5
ScottSavarie <scottsavarie@gmail.com>
2012-11-29 00:18:51 +0100

    css and js updates
    Signed-off-by: ScottSavarie <scottsavarie@gmail.com>

--- assets/js/global.js
+++ assets/js/global.js
@@ -5,8 +5,32 @@
 
 	
 $(document).ready(function() {
-$("#header, #intro").hide();
-$('#header, #intro').fadeIn('slow','linear');
+$("#headline,.center-arrow,.project").hide(0);
+$("#headline").delay(100).fadeIn('slow');
+$(".center-arrow").delay(600).slideDown(300);
+$(".project").show(0);
+
+
 
 }); // end document ready
 
+
+
+/* =Royal Slider
+============================================================================== */
+
+
+jQuery(document).ready(function($) {
+    $(".royalSlider").royalSlider({
+        controlsInside: false,
+        slideSpacing: 0,
+        autoScaleSliderHeight: 450,
+        autoScaleSlider: true,
+        imageScaleMode: 'fill',
+        keyboardNavEnabled: true
+
+
+    });  
+});
+
+// end Royal Slider
